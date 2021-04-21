4c81e6417ebe91b2061045ccc695ebb364ae1124
HungThinh <nguyenhungthinh0937@gmail.com>
2021-04-21 11:47:43 +0700

    Thinh- Dieu khien xe,chuyen gotoXY()+clrscr() qua  Menu.cpp

---
+++ Menu.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<Windows.h>
+
+void Menu()
+{
+    //Phần này làm MENU.
+}
+
+void clrscr()
+{
+    CONSOLE_SCREEN_BUFFER_INFO	csbiInfo;                  
+	HANDLE	hConsoleOut;
+	COORD	Home = {0,0};
+	DWORD	dummy;
+
+	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hConsoleOut,&csbiInfo);
+
+	FillConsoleOutputCharacter(hConsoleOut,' ',csbiInfo.dwSize.X * csbiInfo.dwSize.Y,Home,&dummy);
+	csbiInfo.dwCursorPosition.X = 0;
+	csbiInfo.dwCursorPosition.Y = 0;
+	SetConsoleCursorPosition(hConsoleOut,csbiInfo.dwCursorPosition);
+}
+
+void gotoXY(int x, int y)
+{
+    COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
--- game.cpp
+++ game.cpp
@@ -1,34 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
 #include<Windows.h>
+#include "Menu.cpp"
 
 #define consoleWidth 80
 #define consoleHeight 25
-#define Road 25
 
-void clrscr()
-{
-    CONSOLE_SCREEN_BUFFER_INFO	csbiInfo;                  
-	HANDLE	hConsoleOut;
-	COORD	Home = {0,0};
-	DWORD	dummy;
-
-	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	GetConsoleScreenBufferInfo(hConsoleOut,&csbiInfo);
-
-	FillConsoleOutputCharacter(hConsoleOut,' ',csbiInfo.dwSize.X * csbiInfo.dwSize.Y,Home,&dummy);
-	csbiInfo.dwCursorPosition.X = 0;
-	csbiInfo.dwCursorPosition.Y = 0;
-	SetConsoleCursorPosition(hConsoleOut,csbiInfo.dwCursorPosition);
-}
-
-void gotoXY(int x, int y)
-{
-    COORD coord;
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-}
 
 struct Toado{
     int y, x;
@@ -40,31 +17,24 @@ struct Hinhdang{
 struct Car{
     Toado td;
     Hinhdang hd;
+
 };
 void TaoHinh(Car &xe){
     #define HD xe.hd.a
     xe.td.y = consoleHeight -2;
-    xe.td.x = Road / 2;
+    xe.td.x = consoleWidth / 2;
 
     HD[0][0] = HD[0][2] = HD[2][0] = HD[2][2] = 'o';
     HD[0][1] = '^';
-    HD[1][0] = HD[1][2] = '|'; HD[1][1] = ' ';
+    HD[1][0] = HD[1][2] = '|'; HD[1][1] = 'X';
     HD[2][1]='-';
-    
 }
+
 void Hienthi(Car xe){
     clrscr();
-    for(int i=0;i < consoleHeight; i++)
-        printf("%c\n",179);
-
-    for(int i=0;i < consoleHeight; i++)
-    {
-        gotoXY(Road, i);
-        putchar(179);
-    }
     for(int kDong = -1; kDong <= 1; kDong++)
     {
-        for(int kCot = -1; kCot <=1; kCot++)
+        for(int kCot = -1; kCot <= 1; kCot++)
             {
                 int x = kCot + xe.td.x;
                 int y = kDong + xe.td.y;
@@ -72,19 +42,45 @@ void Hienthi(Car xe){
                 putchar(xe.hd.a[kDong+1][kCot+1]);
             }
     }
+    for(int i=0; i < consoleHeight; i++)
+    {
+        gotoXY(25,i);
+        putchar(179);
+    }
+    for(int i=0; i < consoleHeight; i++)
+    {
+        gotoXY(55, i);
+        putchar(179);
+    }
+    for(int i= 0; i < consoleHeight - 4;i++)
+    {
+        gotoXY(40,i);
+        printf(".\n\n");
+    }
+}
+
+void Dieukhien(Car &xe){
+    if(kbhit())
+    {
+        int key = getch();
+        if(key == 75 && xe.td.x > 27)
+            xe.td.x--;
+        else if(key == 77 && xe.td.x < 53)
+            xe.td.x++;
+    }
 }
 int main()
 {   
     Car xe;
     TaoHinh(xe);
-    while(1){
-        clrscr();
+    while(1)
+    {
         Hienthi(xe);
+        Dieukhien(xe);
 
-        Sleep(500);
+        Sleep(200);
     }
 
 
-    system("pause");
     return 0;
 }
